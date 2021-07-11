0193bc592a568f52d1e1f6ce97966b7243a3c9eb
MaheenK <mk04389@st.habib.edu.pk>
2021-07-11 14:16:27 +0500

    Done till 59:00

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,11 +1,13 @@
 import "../../App.css";
 import mainSection from "../mainSection";
 import React from "react";
+import HeroSection from "../HeroSection";
 
 function Home() {
   return (
     <>
-      <mainSection />
+    <HeroSection/>
+      {/* <mainSection /> */}
     </>
   );
 }
