71fbf723d9501910cdae019444f87eb9f3e0be46
MaheenK <mk04389@st.habib.edu.pk>
2021-11-13 20:33:12 +0500

    donate working

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "./Button";
+import { Link } from "react-router-dom";
 import "./HeroSection.css";
 import "../App.css";
 import vid from "../images/mirrorTrailer2.mp4";
 
 function HeroSection() {
+
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
     <div className="hero-container">
       {/* <image src="logo512.png"/> */}
@@ -13,17 +20,21 @@ function HeroSection() {
       <h1> OUR STORY</h1>
       <p>What are you waiting for</p>
       <div className="hero-btns">
+      
         <Button
           className="btns"
           buttonStyle="btn--outline"
           buttonSize="btn--large"
         >
-          Donate
+            <Link to="/donate" className="linked"  onClick={closeMobileMenu}>
+                 Donate
+              </Link>
         </Button>
         <Button
           className="btns"
           buttonStyle="btn--primary"
           buttonSize="btn--large"
+           onClick={console.log('hey')}
         >
           YouTube <i className="far fa-play-circle" />
         </Button>
