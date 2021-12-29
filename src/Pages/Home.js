a978a523dc671210016d984506d92d15b0c89e55
Rida Khan <49796698+ridakhann@users.noreply.github.com>
2021-12-29 19:30:50 +0500

    fonts added + name and logo meaning fixed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -15,7 +15,7 @@ function Home() {
       <div className="WhyLogo">
         <img src={logo} alt="Logo" />
         <div className="Why-text">
-          <h3 className="heading-logo">What does 'The Mirror' name and logo mean?</h3>
+          <h3 className="heading-logo">The meaning behind our <span>name</span> and <span>logo</span></h3>
           <p>
             The Mirror's name and logo symbolises looking inside a mirror, quite literally, to really see who you are. It was named that because we wanted people to see their own reflections, question who they are and the choices they make. The name is supposed to inspire a sense of self-awareness to look within and do what's right. It can be seen in many perspectives, but we keep the lens focused on all the abusers and we command them to become human, see what their actions can cause and put an end to their henious acts once and for all. Its the power of the mind to realize problems and then fight to make it right.
           </p>
